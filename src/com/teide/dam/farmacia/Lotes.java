8e0c5f2a1a25e702fb36c5311d8dbb3e7348feac
DAM1 <DAM1@PC320>
2014-06-06 10:01:53 +0200

    Añadidas las fechas a los lotes en vez de la medicamento, metodo de alta y fechas al toString

--- src/com/teide/dam/farmacia/Lotes.java
+++ src/com/teide/dam/farmacia/Lotes.java
@@ -4,13 +4,41 @@
  */
 package com.teide.dam.farmacia;
 
+import java.util.GregorianCalendar;
+
 /**
  *
  * @author DAM1
  */
 public class Lotes {
     
+    private String lote, fechaFab, fechaCad;
+    private int unidades;
+    GregorianCalendar gc=new GregorianCalendar();
+
+    public Lotes(String lote, String fechaFab, String fechaCad, int unidades) {
+        this.lote = lote;
+        this.fechaCad=gc.get(GregorianCalendar.DAY_OF_MONTH)+"/"+(gc.get(GregorianCalendar.MONTH)+1)+"/"+(gc.get(GregorianCalendar.YEAR)+1);
+        this.fechaFab=gc.get(GregorianCalendar.DAY_OF_MONTH)+"/"+(gc.get(GregorianCalendar.MONTH)+1)+"/"+(gc.get(GregorianCalendar.YEAR));
+        this.unidades = unidades;
+    }
+
+    public String getLote() {
+        return lote;
+    }
+
+    public int getUnidades() {
+        return unidades;
+    }
     
+    
+    
+    public String getFechaFab() {
+    return fechaFab;
+    }
+    public String getFechaCad() {
+        return fechaCad;
+    }  
     
     
 }
